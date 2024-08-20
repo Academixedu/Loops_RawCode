49a0e730056be88a6e2cbe32261fc18b28e9a747
rohit <rohitsthakre@gmali.com>
2024-08-20 18:41:59 +0530

    done

--- ForL.java
+++ ForL.java
@@ -1,5 +1,16 @@
 public class ForL{
-// Prepare a For Loop Using an Increment Operator 
+  public static void main(String[] args) {
+    // Prepare a For Loop Using an Increment Operator 
+    for (int i = 1; i <=10; i++) {
+      System.out.println("hello"+ i);
+       }
+
   // Prepare a For Loop Using Decrement Operator
+    
+  for (int i = 10; i >=1; i--) {
+    System.out.println("hello"+ i);
+     }
+  }
+
   
 }
