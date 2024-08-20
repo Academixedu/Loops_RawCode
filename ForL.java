7786b7bc237e732361bd361da5fbb5b16efe9122
vijay767492 <vijaydandu59@gmail.com>
2024-08-20 20:12:46 +0530

    loops code

--- ForL.java
+++ ForL.java
@@ -1,5 +1,20 @@
-public class ForL{
-// Prepare a For Loop Using an Increment Operator 
-  // Prepare a For Loop Using Decrement Operator
-  
+import java.util.Scanner;
+public class ForL {
+  public static void main(String[] args) {
+    Scanner ss=new Scanner (System.in);
+    System.out.print("enter number for its table :");
+      int number = ss.nextInt();
+
+      // For loop using increment operator
+      System.out.println("Multiplication table using increment:");
+      for (int i = 1; i <= 10; i++) {
+          System.out.println(number + " x " + i + " = " + (number * i));
+      }
+
+      // For loop using decrement operator
+      System.out.println("\nMultiplication table using decrement:");
+      for (int i = 10; i >= 1; i--) {
+          System.out.println(number + " x " + i + " = " + (number * i));
+      }
+  }
 }
