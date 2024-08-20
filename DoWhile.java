8d3ed6ec021dbaaec8f3cc672e8df3c744c0b688
Vishal <87086820+Vishalprepro@users.noreply.github.com>
2024-08-20 19:45:39 +0530

    Update DoWhile.java

--- DoWhile.java
+++ DoWhile.java
@@ -1,4 +1,12 @@
 public class DoWhile{
 // Prepare a DoWhile Loop Using an Increment Operator 
   // Preapare a DoWhile Loop Using an Decrement Operator
+  public static void main(String args[]){
+    int n=1;
+    do{
+      System.out.println("this is the number's:"+n);
+      n++;
+    }while(n<10);
+
+  }
 }
