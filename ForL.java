29b837f552683b26fc6b6ff133628ddd2c48bb83
Vijay123432 <vijaypenugoneda03@gmail.com>
2024-08-20 18:09:41 +0530

    forloop

--- ForL.java
+++ ForL.java
@@ -2,4 +2,14 @@ public class ForL{
 // Prepare a For Loop Using an Increment Operator 
   // Prepare a For Loop Using Decrement Operator
   
+  public static void main(String[] args) {
+    System.out.println("increase");
+    for(int i=1;i<=5;i++){
+      System.out.println("hello world" +i);
+    }
+    System.out.println("decrease");
+    for(int t=7;t>=1;t--){
+      System.out.println("Hello World" +t);
+    }
+  }
 }
