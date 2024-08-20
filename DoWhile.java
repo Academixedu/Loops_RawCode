bcaac2ac1dc043d92d5043ccb5ff0abe680e4412
Arbaz-01 <arbazshaibu@gmail.com>
2024-08-20 22:09:43 +0530

    arbazz

--- DoWhile.java
+++ DoWhile.java
@@ -1,4 +1,20 @@
 public class DoWhile{
-// Prepare a DoWhile Loop Using an Increment Operator 
+  public static void main(String[] args) {
+    // Prepare a DoWhile Loop Using an Increment Operator 
+    int s=1;
+    do{
+      System.out.println("AWM"+s);
+      s++;
+    }
+    while(s<=8);
+
+System.out.println("Decrement operator");
   // Preapare a DoWhile Loop Using an Decrement Operator
+  int p=8;
+  do {
+    System.out.println("AWM"+p);
+    p--;
+  }
+  while(p>=1);
+}
 }
--- WhileL.java
+++ WhileL.java
@@ -1,4 +1,18 @@
 public class WhileL{
+  public static void main(String[] args) {
+    int z=1;
+    while (z<=5) {
+      System.out.println("Nikki"+z);
+      z++;
+    }
+    System.out.println("decrement");
+
+    int o=8;
+    while (o>=1) {
+      System.out.println("Nikki"+o);
+      o=o-2;
+    }
+  }
 //1) Prepare a While Loop Using ++ operator
 //2) Prepare a While Loop using -- operator
   // Number Should be reduced by 2 on every iteration
