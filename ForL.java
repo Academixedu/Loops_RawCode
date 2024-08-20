ba3a5ba32270632e688cf2512e0dd2a02b293548
Aparna526 <aparnakondapaturu@gmail.com>
2024-08-20 11:39:15 +0530

    Two

--- ForL.java
+++ ForL.java
@@ -1,5 +1,18 @@
+import java.util.*;
 public class ForL{
 // Prepare a For Loop Using an Increment Operator 
   // Prepare a For Loop Using Decrement Operator
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+
+    for(int i=0;i<=n;i++){
+      System.out.println("Increment:"+i);
+    }
+
+    for(int i=n;i>0;i--){
+      System.out.println("Decrement:"+i);
+    }
+  }
   
 }
