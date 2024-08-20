a9aa73992692e52b94b6de83c120bf58a60ebc93
kurojuLucky <luckykuroju28@gmail.com>
2024-08-20 20:28:30 +0530

    assign

--- WhileL.java
+++ WhileL.java
@@ -1,4 +1,16 @@
 public class WhileL{
+  public static void main(String[] args) {
+    int i=1;
+    while (i<7) {
+      System.out.println(+i);
+      i++;
+    }
+    int j=7;
+    while (j>1) {
+      System.out.println(+j);
+      j--;
+    }
+  }
 //1) Prepare a While Loop Using ++ operator
 //2) Prepare a While Loop using -- operator
   // Number Should be reduced by 2 on every iteration
