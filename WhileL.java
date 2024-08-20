0dcac5524688fb9eff4fb93c1246d3bc575f514a
Vishal <87086820+Vishalprepro@users.noreply.github.com>
2024-08-20 19:51:07 +0530

    Update WhileL.java

--- WhileL.java
+++ WhileL.java
@@ -2,6 +2,22 @@ public class WhileL{
 //1) Prepare a While Loop Using ++ operator
 //2) Prepare a While Loop using -- operator
   // Number Should be reduced by 2 on every iteration
+  public static void main(String args[]){
+    int i=0;
+    while(i<10){
+      System.out.println("hello"+i);
+      i=i+2;
+    }
+    int n=10;
+    while(n>0){
+      System.out.println(""+n);
+      n=n-2;
+    }
+    
+
+
+    
+  }
   
   
 }
