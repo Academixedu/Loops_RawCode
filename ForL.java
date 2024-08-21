65f78e84f0b2704c3147442f15be1679b79baf7e
SMohammadKhan <iammohammad65@gmail.com>
2024-08-21 10:29:16 +0530

    normal

--- ForL.java
+++ ForL.java
@@ -1,5 +1,8 @@
 public class ForL{
-// Prepare a For Loop Using an Increment Operator 
-  // Prepare a For Loop Using Decrement Operator
+  public static void main(String[] args) {
+    // Prepare a For Loop Using an Increment Operator 
+  // Prepare a For Loop Using Decrement Operators
+
+  }
   
 }
